66daa606571d4870084b7578720cf2465a60fed0
Marko Klobucar Ledinscak <mrqoqlobo@gmail.com>
2024-10-07 23:36:35 +0200

    chore: Initial setup

--- lib/lexer/lexer.rs
+++ lib/lexer/lexer.rs
@@ -1,14 +1,106 @@
+pub enum Token {
+    // Types
+    Int8,
+    Int16,
+    Int32,
+    Int64,
+    Int,
+    Uint8,
+    Uint16,
+    Uint32,
+    Uint64,
+    Uint,
+    Float32,
+    Float64,
+    Float,
+    Bool,
+    String,
+    Char,
+
+    // Keywords
+    Func,
+    Struct,
+    Method,
+    Enum,
+    If,
+    Else,
+    Match,
+    Field,
+    Constrained,
+    New,
+    Return,
+    Protocol,
+
+    // Math operators
+    Plus,
+    Minus,
+    Mul,
+    Div,
+    Mod,
+
+    // Logic operators
+    Not,
+    And,
+    Or,
+    Eq,
+    Ne,
+    Lt,
+    Le,
+    Gt,
+    Ge,
+
+    // Assignment
+    Assign,      // =
+    InferAssign, // :=
+    TypeDecl,    // :
+
+    // Punctuation
+    OpenHashBrace,
+    CloseHashBrace,
+    OpenList,
+    CloseList,
+    OpenGenericBracket,
+    CloseGenericBracker,
+    Comma,
+    Whitespace,
+    DoubleColon,
+
+    // Identifiers
+    Variable(String),
+    FunctionName(String),
+    MethodName(String),
+    ProtocolName(String),
+
+    // Literals
+    // TODO(tefached95): Remove?
+    IntLiteral(i64),
+    UintLiteral(u64),
+    FloatLiteral(f64),
+    StringLiteral(String),
+    CharLiteral(char),
+
+    // Misc
+    LineComment(String),  // -- This is a comment
+    BlockComment(String), // {- This is a comment -}
+    EOF,
+    EOL,
+}
+
+#[derive(Debug)]
 pub struct Lexer {
     source: String,
-    position: usize,
+    line: usize,
+    column: usize,
+    indentation_stack: Vec<usize>,
 }
 
 impl Lexer {
     pub fn new(source: String) -> Self {
-        Self { source, position: 0 }
-    }
-
-    pub fn say_hello<'a>(&self, what_to_say: &'a str) -> &'a str {
-        what_to_say
+        Self {
+            source,
+            line: 1,
+            column: 1,
+            indentation_stack: vec![0],
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
-use petram::lexer::Lexer;
+use std::path;
 
-fn main() {
-    let lx = Lexer::new("../examples/hello_world.petra".to_string());
-    println!("{}", lx.say_hello("Hello, Petram!"));
+use clap::Parser;
+use petram::lexer;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long)]
+    program: String,
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let args = Args::parse();
+
+    let program_path = path::Path::new(args.program.as_str())
+        .canonicalize()
+        .expect("Could not canonicalize path");
+
+    println!("Program path: {}", program_path.display());
+
+    if let Ok(source) = std::fs::read_to_string(program_path) {
+        let lexer = lexer::Lexer::new(source);
+        println!("{:#?}", lexer);
+        return Ok(());
+    } else {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "Could not read file",
+        ));
+    }
 }
