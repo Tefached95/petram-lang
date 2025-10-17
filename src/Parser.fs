2535c8a4fa28867fd8589ac1e09f058cb3554b79
Marko Klobučar Ledinšćak <mrqoqlobo@gmail.com>
2025-10-17 14:51:56 +0200

    Move types to `Types.fs`

--- src/Parser.fs
+++ src/Parser.fs
@@ -3,24 +3,6 @@ module Parser
 open Tokens
 open Types
 
-type Parameter = { Name: string; Type: TypeAnnotation }
-
-type Expression =
-    | IntLiteral of int
-    | StringLiteral of string
-    | Identifier of string
-    | FunctionCall of name: string * arguments: (string * Expression) list
-
-type Statement =
-    | ExpressionStatement of Expression
-    | Return of Expression
-
-type FunctionDeclaration =
-    { Name: string
-      Parameters: Parameter list option
-      ReturnType: TypeAnnotation option
-      Body: Statement list }
-
 let rec parseTypeAnnotation (tokens: Token list) : TypeAnnotation * Token list =
     match tokens with
     | Token.Identifier typeName :: LeftAngleBracket :: rest ->
--- src/Types.fs
+++ src/Types.fs
@@ -3,3 +3,21 @@ module Types
 type TypeAnnotation =
     | SimpleType of string
     | GenericType of name: string * typeArgs: TypeAnnotation list
+
+type Parameter = { Name: string; Type: TypeAnnotation }
+
+type Expression =
+    | IntLiteral of int
+    | StringLiteral of string
+    | Identifier of string
+    | FunctionCall of name: string * arguments: (string * Expression) list
+
+type Statement =
+    | ExpressionStatement of Expression
+    | Return of Expression
+
+type FunctionDeclaration =
+    { Name: string
+      Parameters: Parameter list option
+      ReturnType: TypeAnnotation option
+      Body: Statement list }
