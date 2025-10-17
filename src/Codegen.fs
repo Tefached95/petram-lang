04b37fe8aa99866a59bdcb762596a569619c4133
Marko Klobučar Ledinšćak <mrqoqlobo@gmail.com>
2025-10-17 14:51:49 +0200

    feat: Start working on Codegen

---
+++ src/Codegen.fs
@@ -0,0 +1,14 @@
+module Codegen
+
+open Types
+
+let emitExpression (expr: Expression) : string =
+    failwith "TODO: implement `emitExpression`"
+
+let emitStatement (statement: Statement) : string =
+    failwith "TODO: implement `emitStatement`"
+
+let emitFunction (fn: FunctionDeclaration) : string =
+    failwith "TODO: implement `emitFunction`"
+
+let emit (fn: FunctionDeclaration) : string = failwith "TODO: implement `emit`"
