393486152b2aeb7f50975ac1a4e921a80c6536bd
Marko Klobučar Ledinšćak <mrqoqlobo@gmail.com>
2025-10-21 17:34:26 +0200

    feat: Refactor type mapping to use a lookup table for simple types

--- src/Codegen.fs
+++ src/Codegen.fs
@@ -5,23 +5,8 @@ open Types
 let rec emitType (typeAnnotation: TypeAnnotation) : string =
     match typeAnnotation with
     | SimpleType t ->
-        match t with
-        | "Int" -> "int64_t"
-        | "int8" -> "int8_t"
-        | "int16" -> "int16_t"
-        | "int32" -> "int32_t"
-        | "int64" -> "int64_t"
-        | "uint" -> "uint32_t"
-        | "uint8" -> "uint8_t"
-        | "uint16" -> "uint16_t"
-        | "uint32" -> "uint32_t"
-        | "uint64" -> "uint64_t"
-        | "Float" -> "double"
-        | "float32" -> "float"
-        | "float64" -> "double"
-        | "String" -> "char*"
-        | "()" -> "void"
-        | t -> failwith $"Unexpected simple return type {t}"
+        Map.tryFind t petramToCTypeMap
+        |> Option.defaultWith (fun () -> failwith $"Could not map type {t} to a C equivalent.")
     | GenericType(name, ts) -> failwith $"TODO: Return generic type {name}: {ts}"
 
 let rec emitExpression (expr: Expression) : string =
--- src/Types.fs
+++ src/Types.fs
@@ -24,3 +24,21 @@ type FunctionDeclaration =
       Parameters: Parameter list option
       ReturnType: TypeAnnotation option
       Body: Statement list }
+
+let petramToCTypeMap =
+    Map.ofList
+        [ "Int", "int64_t"
+          "int8", "int8_t"
+          "int16", "int16_t"
+          "int32", "int32_t"
+          "int64", "int64_t"
+          "uint", "uint32_t"
+          "uint8", "uint8_t"
+          "uint16", "uint16_t"
+          "uint32", "uint32_t"
+          "uint64", "uint64_t"
+          "Float", "double"
+          "float32", "float"
+          "float64", "double"
+          "String", "char*"
+          "()", "void" ]
